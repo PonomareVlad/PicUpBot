8a5c8e15322ea2b5ed627fb5565f6cd45fa80798
Vladislav Ponomarev <PonomareVlad@users.noreply.github.com>
2025-02-09 23:29:06 +0100

    Add chat photo setup functionality with inline keyboard
    Introduced the ability to set chat photos using an interactive flow. Implemented `StatelessQuestion` integration for handling photo uploads and ensured error handling for a smoother user experience. Updated dependencies and configurations accordingly.

--- src/bot.mjs
+++ src/bot.mjs
@@ -1,17 +1,51 @@
-import { Bot } from 'grammy'
+import { Bot, InlineKeyboard, InputFile, Keyboard } from 'grammy'
+import { StatelessQuestion } from '@grammyjs/stateless-question'
 
 export const {
-
-    // Telegram bot token from t.me/BotFather
     TELEGRAM_BOT_TOKEN: token,
-
-    // Secret token to validate incoming updates
     TELEGRAM_SECRET_TOKEN: secretToken = String(token).split(':').pop(),
-
 } = process.env
 
-// Default grammY bot instance
 export const bot = new Bot(token)
 
-// Sample handler for a simple echo bot
-bot.on('message:text', ctx => ctx.reply(ctx.msg.text))
+const safe = bot.errorBoundary(console.error)
+
+const start = ctx =>
+    ctx.reply('В каком чате установить изображение ?', {
+        reply_markup: new Keyboard().requestChat('Выберите чат', 0, {
+            user_administrator_rights: { can_change_info: true },
+            bot_administrator_rights: { can_change_info: true },
+        }),
+    })
+
+const picture = (ctx, chat_id = ctx.msg.chat_shared.chat_id.toString()) =>
+    pictureQuestion.replyWithMarkdownV2(
+        ctx,
+        'Отправьте изображение как документ',
+        chat_id
+    )
+
+const pictureQuestion = new StatelessQuestion(
+    'picture',
+    async (ctx, chat_id) => {
+        if (!ctx.has('msg:document')) return picture(ctx, chat_id)
+        try {
+            const { file_path } = await ctx.getFile()
+            const url = `https://api.telegram.org/file/bot${token}/${file_path}`
+            await ctx.api.setChatPhoto(
+                parseInt(chat_id),
+                new InputFile({ url })
+            )
+            await ctx.api.leaveChat(parseInt(chat_id))
+            await ctx.reply('Готово')
+            await start(ctx)
+        } catch (e) {
+            await ctx.reply(e.message)
+            await picture(ctx, chat_id)
+        }
+    }
+)
+
+safe.use(pictureQuestion.middleware())
+safe.on('message:chat_shared', ctx => picture(ctx))
+safe.on('message:text', start)
